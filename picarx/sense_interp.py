486b04fc37bb707788dbc85c080f8487c124b1b6
Ashutosh Gupta <59316418+Ashutosh781@users.noreply.github.com>
2024-01-28 00:17:12 +0000

    Sensing class complete

--- picarx/sense_interp.py
+++ picarx/sense_interp.py
@@ -1,19 +1,37 @@
 import time
 import numpy as np
-from picarx_improved import Picarx
+import logging
+import atexit
+
+try:
+    from robot_hat import ADC, Grayscale_Module
+except ImportError:
+    from sim_robot_hat import ADC, Grayscale_Module
 
 
-class Sensing(Picarx):
+class Sensing(object):
     """Class to get sensor data from Grayscale module on PiCar-X"""
 
-    def __init__(self):
-        super().__init__()
+    GRAYSCALE_PINS = ['A0', 'A1', 'A2']
 
-    def get_grayscale(self):
+    def __init__(self):
+        self.ch0, self.ch1, self.ch2 = [ADC(pin) for pin in self.GRAYSCALE_PINS]
+
+    def get_grayscale_data(self, is_normal:bool=True):
         """Function to get the values"""
 
-        data = self.get_grayscale_data()
-        print(f"Current data {data}")
+        # Read the data
+        data = [self.ch0.read(), self.ch1.read(), self.ch2.read()]
+
+        # Normalize the data
+        if is_normal:
+            # Normalize the data with Normal Distribution
+            data = (data - np.mean(data)) / np.std(data)
+        else:
+            # Normalize the data with just mean
+            data = data / np.mean(data)
+
+        return data
 
 
 class Interpret(Sensing):
@@ -30,12 +48,13 @@ class Interpret(Sensing):
 
 if __name__ == "__main__":
 
-    sense = Sensing()
+    # Testing sensing module
+    sensor = Sensing()
 
     try:
         while True:
-            sense.get_grayscale()
+            data= sensor.get_grayscale_data()
+            print(f"Data: {data}")
             time.sleep(1)
     except KeyboardInterrupt:
-        sense.stop()
         print("Program stopped by User")
