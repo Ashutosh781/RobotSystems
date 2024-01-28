4edb5cd1aa81d7b0982c29cd38dabe95ea9ee622
Ashutosh Gupta <59316418+Ashutosh781@users.noreply.github.com>
2024-01-27 17:47:37 -0800

    Interpret class started

--- picarx/sense_interp.py
+++ picarx/sense_interp.py
@@ -4,9 +4,9 @@ import logging
 import atexit
 
 try:
-    from robot_hat import ADC, Grayscale_Module
+    from robot_hat import ADC
 except ImportError:
-    from sim_robot_hat import ADC, Grayscale_Module
+    from sim_robot_hat import ADC
 
 
 class Sensing(object):
@@ -26,24 +26,48 @@ class Sensing(object):
         # Normalize the data
         if is_normal:
             # Normalize the data with Normal Distribution
-            data = (data - np.mean(data)) / np.std(data)
+            data = (data - np.mean(data)) / (np.std(data) + 1e-9) # Add a small value to avoid division by zero
         else:
             # Normalize the data with just mean
-            data = data / np.mean(data)
+            data = data / (np.mean(data) + 1e-9) # Add a small value to avoid division by zero
 
         return data
 
 
-class Interpret(Sensing):
-    """Class to interpret sensor data from Grayscale module on PiCar-X"""
+class Interpret(object):
+    """Class to interpret sensor data from Grayscale module on PiCar-X
 
-    def __init__(self):
-        super().__init__()
+    Args:
+        l_th: Threshold for slight turn
+        h_th: Threshold for hard turn
+        polarity: Polarity of the line (1 for black line on white background, -1 for white line on black background)
+    """
 
-    def interpret_grayscale(self):
-        """Function to interpret the values"""
+    def __init__(self, l_th:float=0.5, h_th:float=1.0, polarity:int=1):
 
-        pass
+        # Set the thresholds
+        self.l_th = l_th
+        self.h_th = h_th
+
+        # Set the polarity
+        self.polarity = polarity
+
+        # Intialize the sensing module
+        self.sensor = Sensing()
+
+    def get_direction(self):
+        """Function to get direction and degree of turn based on sensor data"""
+
+        # Get the sensor data
+        data = self.sensor.get_grayscale_data() # (3x1)
+
+        # Take difference between sensor data to get edge
+        edge = np.diff(data) # (2x1)
+        edge[1] *= -1 # To get edge values of extreme sensors wrt to center sensor
+        edge_val = np.abs(edge)
+        edge_sign = np.sign(edge)
+
+        # Get the direction of turn
 
 
 if __name__ == "__main__":
